52126a9f0b93c8dccf4edb609ed63d769f588591
UllestReal <119653751+UllestReal@users.noreply.github.com>
2024-04-24 14:17:54 +0200

    format

--- src/events/editep.js
+++ src/events/editep.js
@@ -1,16 +1,16 @@
 const { EmbedBuilder, Events, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const Discord = require('discord.js');
-const {epModel} = require('../Schemas/ep');
-let {logchannelModel, Channel} = require('../Schemas/logchannel');
+const { epModel } = require('../Schemas/ep');
+let { logchannelModel, Channel } = require('../Schemas/logchannel');
 module.exports = {
     name: Events.InteractionCreate,
-    async execute (interaction, client) {
-/* Name: String,
-    Guild: String,
-    Sheetid: String,
-    Range: String,
-    Weeklyoffset: Number,
-    Totaloffset: Number,*/
+    async execute(interaction, client) {
+        /* Name: String,
+            Guild: String,
+            Sheetid: String,
+            Range: String,
+            Weeklyoffset: Number,
+            Totaloffset: Number,*/
         if (!interaction.guild) return;
         if (!interaction.isModalSubmit()) return;
 
@@ -34,41 +34,41 @@ module.exports = {
 
             var logdata = await logchannelModel.find({ Guild: interaction.guild.id });
             var values = [];
-                        await logdata.forEach(async value => {
-                            if (!value.Channel) return;
-                            else {
-                               
-                                values.push( Channel = value.Channel);
-                            }
-                        });
+            await logdata.forEach(async value => {
+                if (!value.Channel) return;
+                else {
 
-                        const id = interaction.user.id;
-                        const iconURL = interaction.guild.iconURL({ dynamic: true });
-                        const guildname = interaction.guild.name;
-                        
-                        const embed = new EmbedBuilder()
-                            .setColor("Blurple")
-                            .setTitle(`EP Configuration Updated!`)
-                            .addFields(
-                                { name: "Command Issuer", value: `<@${id}>`},
-                                { name: "New Range", value: `${range}`},
-                                { name: "New Sheetid", value: `${sheetid}`},
-                                { name: `New Weekly offset`, value: `${weeklyoffset}`},
-                                { name: `New Total offset`, value: ` ${totaloffset}`}
-                            )
-                            .setTimestamp()
-                            .setFooter({ text: guildname, iconURL: iconURL });
-    // Send the log message to the log channel
-    const guild = interaction.guild;
-     const logChannel = guild.channels.cache.get(Channel);
-    if (logChannel instanceof Discord.TextChannel) { 
-      await logChannel.send({ embeds: [embed] }).catch(err => {});
-    }
-           
+                    values.push(Channel = value.Channel);
+                }
+            });
 
-            
+            const id = interaction.user.id;
+            const iconURL = interaction.guild.iconURL({ dynamic: true });
+            const guildname = interaction.guild.name;
 
-        
+            const embed = new EmbedBuilder()
+                .setColor("Blurple")
+                .setTitle(`EP Configuration Updated!`)
+                .addFields(
+                    { name: "Command Issuer", value: `<@${id}>` },
+                    { name: "New Range", value: `${range}` },
+                    { name: "New Sheetid", value: `${sheetid}` },
+                    { name: `New Weekly offset`, value: `${weeklyoffset}` },
+                    { name: `New Total offset`, value: ` ${totaloffset}` }
+                )
+                .setTimestamp()
+                .setFooter({ text: guildname, iconURL: iconURL });
+            // Send the log message to the log channel
+            const guild = interaction.guild;
+            const logChannel = guild.channels.cache.get(Channel);
+            if (logChannel instanceof Discord.TextChannel) {
+                await logChannel.send({ embeds: [embed] }).catch(err => { });
+            }
+
+
+
+
+
             await interaction.reply({ content: `✅ The EP Config has successfully been updated!`, ephemeral: true });
         }
 
